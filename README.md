0ec59235eff425d262fc918845f07ab6ad303552
Luciano Alves <luas10c@gmail.com>
2025-01-28 03:02:03 -0300

    chore: first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Boilerplate React.js Vite
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import './globals.css'
+
+export function App() {
+  return (
+    <section>
+      <div>
+        <h2>Hello, World!</h2>
+      </div>
+    </section>
+  )
+}
